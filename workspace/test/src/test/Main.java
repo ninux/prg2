d01e9ccf9248ba919a47ed91d89e1897ac15c3b7
Nino Ninux <nino.ninux@gmail.com>
2014-02-16 13:25:27 +0100

    testfile ergänzt

--- workspace/test/src/test/Main.java
+++ workspace/test/src/test/Main.java
@@ -1,7 +1,21 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This is a part of the "PRG2" source from the github repository prg2
+ * which is available at http://github.com/ninux/prg2
+ *
+ * Copyright (C) 2014, Ervin Mazlagic (aka. ninux, Nino Ninux)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package test;
@@ -12,7 +26,13 @@ package test;
  */
 public class Main {
     static public void main(String[] args){
-        System.out.println("This is a test. If you can read this, everythings"
-                + " just fine");
+    	String copyright = "PRG2 Copyright (C) 2014 Ervin Mazlagic";
+	String warranty = "This program comes with ABSOLUTELY NO WARRANTY.";
+	String license = "This is free software as defined by GPLv3. \n"
+		+ "You can modify and redistribute it under "
+		+ "certain conditions (see GPLv3).";
+	String hiGPL = (copyright + "\n" + warranty + "\n" + license);
+
+	System.out.println(hiGPL);
     }
-}
+} 
