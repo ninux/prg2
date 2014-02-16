fcd4902718f985c7feffd70aa7fc332e55f0a121
Nino Ninux <nino.ninux@gmail.com>
2014-02-16 11:55:31 +0100

    initial commit

---
+++ workspace/test/src/test/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+/**
+ * 
+ * @author ninux
+ */
+public class Main {
+    static public void main(String[] args){
+        System.out.println("This is a test. If you can read this, everythings"
+                + " just fine");
+    }
+}
