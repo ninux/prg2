5c45c65c552dfd31c9f80bdf0b9df7c9cb3ebf5f
Nino Ninux <nino.ninux@gmail.com>
2014-03-24 16:22:22 +0100

    solved exercise 2.1 from oop5

---
+++ workspace/swings/src/swings/Swings.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package swings;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author ninux
+ */
+public class Swings extends JFrame{
+
+	private int numClicks = 0;
+	
+	public static void main(String[] args){
+		JFrame frame = new Swings();
+	}
+	
+	public Swings(){
+		super("Swings");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		JButton button = new JButton("Count button clicks");
+		final JLabel label = new JLabel("Number of clicks: " + numClicks);
+				
+		button.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent event){
+					incrementClicks();
+					label.setText("Number of clicks: " + numClicks);
+				}
+			}
+		);
+		
+		setLayout(new GridLayout(0, 1));
+		add(button);
+		add(label);
+		
+		pack();
+		setVisible(true);
+	}
+	
+	private void incrementClicks(){
+		numClicks++;	
+	}
+}
